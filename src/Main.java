dd76e66726435f6d53c86bf30a297921fce62b33
JaxonZaleski <zaleskjn@mail.uc.edu>
2024-09-27 11:35:19 -0400

    Initial commit plus initial code

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        double itemCost = 0.00;
+        double shippingCost = 1.02;
+
+        System.out.println("Enter the price of the item: ");
+        itemCost=in.nextDouble();
+
+    }
+}
