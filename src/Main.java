5d8f9c00b9e194f88683aa3071c2551711617172
JaxonZaleski <zaleskjn@mail.uc.edu>
2024-09-27 12:08:32 -0400

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,15 @@ public class Main {
         double shippingCost = 1.02;
 
         System.out.println("Enter the price of the item: ");
-        itemCost=in.nextDouble();
-
+        itemCost = in.nextDouble();
+        in.nextLine();
+         if (itemCost >= 100){
+             System.out.println("Shipping is free. Item cost is " + itemCost);
+         } else if (itemCost <=100 && itemCost >=1){
+             itemCost = itemCost * shippingCost;
+             System.out.println("The total item cost is " + itemCost);
+         } else {
+             System.out.println("You have entered the wrong value");
+         }
     }
 }
